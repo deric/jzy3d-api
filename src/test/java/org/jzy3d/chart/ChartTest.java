0027fff7c0d41ef501eb75a661e3c9c71241a762
Tomas Barton <barton.tomas@gmail.com>
2013-03-23 20:18:39 +0100

    temporarily removed tests

--- src/test/java/org/jzy3d/chart/ChartTest.java
+++ src/test/java/org/jzy3d/chart/ChartTest.java
@@ -7,232 +7,336 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
-
-
-import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
-
-import javax.imageio.ImageIO;
-
-import junit.framework.TestCase;
-
-import org.apache.log4j.Logger;
-import org.jzy3d.chart.Chart;
-
 /**
- * Primitives for chart tests.
- * 
- * A chart having a screenshot different from the test case
- * image will generate a call to {@link org.junit.Assert.fail}
- * 
- * @author martin
+ *
+ * @author deric
  */
-public class ChartTest extends TestCase{
-    public static ChartTest tester = new ChartTest();
+public class ChartTest {
     
-    public static void assertSimilar(Chart chart, String testImage){
-        try {
-            tester.execute(chart, testImage);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        assertTrue(testImage, true);
+    public ChartTest() {
     }
-    /*
-     * In Java, a mouse click only registers if the mouse is pressed and
-     * released without moving the mouse at all. This is difficult for most
-     * users to accomplish, so most UI elements (like buttons) react to the
-     * mouse press and release events and ignore the "click".
-     */
-
-    /**
-     * Run a chart and verify if its screenshot is pixelwise similar to the test
-     * case image.
-     * 
-     * If test case image does not exist, build it for the first time.
-     * 
-     * Failure to compare the chart with the test case image will create an
-     * image <code>data/tests/error-[name].png</code>. This image is always
-     * deleted before running a testcase.
-     * 
-     * Calling clean() will delete the test case image.
-     * 
-     * @param chart
-     * @param testImage
-     * @throws IOException
-     *             if a non chart related error occurs. Actual chart test errors
-     *             call <code>fail(...)</code>
-     */
-    public void execute(Chart chart, String testImage) throws IOException {
-        clean(getTestCaseFailedFileName());
-
-        if (!isBuilt(testImage))
-            build(chart, testImage);
-        test(chart, testImage);
+    
+    @BeforeClass
+    public static void setUpClass() {
     }
-
-    public void execute(Chart chart) throws IOException {
-        execute(chart, getTestCaseFileName());
+    
+    @AfterClass
+    public static void tearDownClass() {
     }
-
-    public void execute(BufferedImage image, String testImage) throws IOException {
-        clean(getTestCaseFailedFileName());
-
-        if (!isBuilt(testImage))
-            build(image, testImage);
-        test(image, testImage);
+    
+    @Before
+    public void setUp() {
     }
-
-    public void execute(BufferedImage image) throws IOException {
-        execute(image, getTestCaseFileName());
-    }
-
-    /* Primitives to test images */
-
-    public void clean(String testImage) {
-        if (!cleanFile(testImage) && isBuilt(testImage))
-            Logger.getLogger(ChartTest.class).warn("test case file not cleaned: " + testImage);
-    }
-
-    public boolean cleanFile(String file) {
-        return new File(file).delete();
-    }
-
-    public void build(Chart chart, String testImage) throws IOException {
-        Logger.getLogger(ChartTest.class).warn("building the screenshot to assert later as no test image is available: " + testImage);
-        screenshot(chart, testImage);
-    }
-
-    public void build(BufferedImage image, String testImage) throws IOException {
-        Logger.getLogger(ChartTest.class).warn("saving the image to assert later as no test image is available: " + testImage);
-
-        screenshot(image, testImage);
-    }
-
-    public boolean isBuilt(String testImage) {
-        return new File(testImage).exists();
-    }
-
-    public void test(Chart chart, String testImage) throws IOException {
-        try {
-            Logger.getLogger(ChartTest.class).info("compare chart with " + testImage);
-            compare(chart, testImage);
-        } catch (IOException e) {
-            fail("IOException: " + e.getMessage());
-        } catch (ChartTestFailed e) {
-            screenshot(chart, testImage.replace(".", "#ERROR#."));
-            fail("Chart test failed: " + e.getMessage() + " see " + testImage);
-        }
-    }
-
-    public void test(BufferedImage image, String testImage) throws IOException {
-        try {
-            Logger.getLogger(ChartTest.class).info("compare chart with " + testImage);
-            compare(image, testImage);
-        } catch (IOException e) {
-            fail("IOException: " + e.getMessage());
-        } catch (ChartTestFailed e) {
-            fail("Image test failed: " + e.getMessage() + " see " + testImage);
-        }
-    }
-
-    /* */
-
-    public void compare(Chart chart, String filename) throws IOException, ChartTestFailed {
-        BufferedImage i = chart.screenshot();
-
-        if (i != null) {
-            compare(i, filename);
-        } else {
-            throw new RuntimeException("screenshot not available");
-        }
-    }
-
-    public void compare(BufferedImage i, String filename) throws IOException, ChartTestFailed {
-        BufferedImage i2 = ImageIO.read(new File(filename));
-        compare(i, i2);
+    
+    @After
+    public void tearDown() {
     }
 
     /**
-     * Compare images pixel-wise.
-     * 
-     * @param i1
-     * @param i2
-     * @throws ChartTestFailed
-     *             as soon as a pixel difference can be found
+     * Test of display method, of class Chart.
      */
-    public void compare(BufferedImage i1, BufferedImage i2) throws ChartTestFailed {
-        // int rbg = image.getRGB((int) x, (maxRow) - ((int) y));
-
-        int i1W = i1.getWidth();
-        int i1H = i1.getHeight();
-        int i2W = i2.getWidth();
-        int i2H = i2.getHeight();
-
-        if (i1W == i2W && i1H == i2H) {
-            for (int i = 0; i < i1W; i++) {
-                for (int j = 0; j < i1H; j++) {
-                    int p1rgb = i1.getRGB(i, j);
-                    int p2rgb = i2.getRGB(i, j);
-                    if (p1rgb != p2rgb) {
-                        String m = "pixel diff @(" + i + "," + j + ")";
-                        throw new ChartTestFailed(m, i1, i2);
-                    }
-                }
-            }
-        } else {
-            String m = "image size differ: i1={" + i1W + "," + i1H + "} i2={" + i2W + "," + i2H + "}";
-            throw new ChartTestFailed(m);
-        }
+    @Test
+    public void testDisplay() {
     }
 
-    /* */
-
-    public void screenshot(Chart chart, String filename) throws IOException {
-        screenshot(chart.screenshot(), filename);
+    /**
+     * Test of clear method, of class Chart.
+     */
+    @Test
+    public void testClear() {
     }
 
-    public void screenshot(BufferedImage image, String testImage) throws IOException {
-        File output = new File(testImage);
-        if (!output.getParentFile().exists())
-            output.mkdirs();
-        ImageIO.write(image, "png", output);
+    /**
+     * Test of stopAnimator method, of class Chart.
+     */
+    @Test
+    public void testStopAnimator() {
     }
 
-    /* */
-
-    public File getTestCaseFile() {
-        return new File(getTestCaseFileName());
+    /**
+     * Test of dispose method, of class Chart.
+     */
+    @Test
+    public void testDispose() {
     }
 
-    public String getTestCaseFileName() {
-        return getTestCaseFileName(getTestName());
+    /**
+     * Test of render method, of class Chart.
+     */
+    @Test
+    public void testRender() {
     }
 
-    public String getTestCaseFileName(String testName) {
-        return getTestCaseFolder() + testName + ".png";
+    /**
+     * Test of screenshot method, of class Chart.
+     */
+    @Test
+    public void testScreenshot_0args() {
     }
 
-    public String getTestCaseFailedFileName() {
-        return getTestCaseFolder() + "error-" + getTestName() + ".png";
+    /**
+     * Test of screenshot method, of class Chart.
+     */
+    @Test
+    public void testScreenshot_String() throws Exception {
     }
 
-    public String getTestCaseFolder() {
-        return "data/tests/";
+    /**
+     * Test of updateProjectionsAndRender method, of class Chart.
+     */
+    @Test
+    public void testUpdateProjectionsAndRender() {
     }
 
-    public String getTestName() {
-        return this.getClass().getSimpleName();
+    /**
+     * Test of newView method, of class Chart.
+     */
+    @Test
+    public void testNewView() {
     }
 
-    public String getTestCanvasType() {
-        return "offscreen, " + WIDTH + ", " + HEIGHT;
+    /**
+     * Test of addMouseController method, of class Chart.
+     */
+    @Test
+    public void testAddMouseController() {
     }
 
-    /* */
+    /**
+     * Test of addKeyController method, of class Chart.
+     */
+    @Test
+    public void testAddKeyController() {
+    }
 
-    // int bufImgType = BufferedImage.TYPE_3BYTE_BGR;// );
-    protected int WIDTH = 800;
-    protected int HEIGHT = 600;
+    /**
+     * Test of addScreenshotKeyController method, of class Chart.
+     */
+    @Test
+    public void testAddScreenshotKeyController() {
+    }
+
+    /**
+     * Test of open method, of class Chart.
+     */
+    @Test
+    public void testOpen_3args() {
+    }
+
+    /**
+     * Test of open method, of class Chart.
+     */
+    @Test
+    public void testOpen_String_Rectangle() {
+    }
+
+    /**
+     * Test of addController method, of class Chart.
+     */
+    @Test
+    public void testAddController() {
+    }
+
+    /**
+     * Test of removeController method, of class Chart.
+     */
+    @Test
+    public void testRemoveController() {
+    }
+
+    /**
+     * Test of clearControllerList method, of class Chart.
+     */
+    @Test
+    public void testClearControllerList() {
+    }
+
+    /**
+     * Test of getControllers method, of class Chart.
+     */
+    @Test
+    public void testGetControllers() {
+    }
+
+    /**
+     * Test of addDrawable method, of class Chart.
+     */
+    @Test
+    public void testAddDrawable_AbstractDrawable() {
+    }
+
+    /**
+     * Test of addDrawable method, of class Chart.
+     */
+    @Test
+    public void testAddDrawable_AbstractDrawable_boolean() {
+    }
+
+    /**
+     * Test of addDrawable method, of class Chart.
+     */
+    @Test
+    public void testAddDrawable_List_boolean() {
+    }
+
+    /**
+     * Test of addDrawable method, of class Chart.
+     */
+    @Test
+    public void testAddDrawable_List() {
+    }
+
+    /**
+     * Test of removeDrawable method, of class Chart.
+     */
+    @Test
+    public void testRemoveDrawable_AbstractDrawable() {
+    }
+
+    /**
+     * Test of removeDrawable method, of class Chart.
+     */
+    @Test
+    public void testRemoveDrawable_AbstractDrawable_boolean() {
+    }
+
+    /**
+     * Test of addRenderer method, of class Chart.
+     */
+    @Test
+    public void testAddRenderer() {
+    }
+
+    /**
+     * Test of removeRenderer method, of class Chart.
+     */
+    @Test
+    public void testRemoveRenderer() {
+    }
+
+    /**
+     * Test of addLight method, of class Chart.
+     */
+    @Test
+    public void testAddLight_Coord3d() {
+    }
+
+    /**
+     * Test of addLight method, of class Chart.
+     */
+    @Test
+    public void testAddLight_5args() {
+    }
+
+    /**
+     * Test of getView method, of class Chart.
+     */
+    @Test
+    public void testGetView() {
+    }
+
+    /**
+     * Test of getScene method, of class Chart.
+     */
+    @Test
+    public void testGetScene() {
+    }
+
+    /**
+     * Test of getCanvas method, of class Chart.
+     */
+    @Test
+    public void testGetCanvas() {
+    }
+
+    /**
+     * Test of getAxeLayout method, of class Chart.
+     */
+    @Test
+    public void testGetAxeLayout() {
+    }
+
+    /**
+     * Test of setAxeDisplayed method, of class Chart.
+     */
+    @Test
+    public void testSetAxeDisplayed() {
+    }
+
+    /**
+     * Test of getFactory method, of class Chart.
+     */
+    @Test
+    public void testGetFactory() {
+    }
+
+    /**
+     * Test of getWindowingToolkit method, of class Chart.
+     */
+    @Test
+    public void testGetWindowingToolkit() {
+    }
+
+    /**
+     * Test of setViewPoint method, of class Chart.
+     */
+    @Test
+    public void testSetViewPoint() {
+    }
+
+    /**
+     * Test of getViewPoint method, of class Chart.
+     */
+    @Test
+    public void testGetViewPoint() {
+    }
+
+    /**
+     * Test of setViewMode method, of class Chart.
+     */
+    @Test
+    public void testSetViewMode() {
+    }
+
+    /**
+     * Test of getViewMode method, of class Chart.
+     */
+    @Test
+    public void testGetViewMode() {
+    }
+
+    /**
+     * Test of setScale method, of class Chart.
+     */
+    @Test
+    public void testSetScale_Scale_boolean() {
+    }
+
+    /**
+     * Test of setScale method, of class Chart.
+     */
+    @Test
+    public void testSetScale_Scale() {
+    }
+
+    /**
+     * Test of getScale method, of class Chart.
+     */
+    @Test
+    public void testGetScale() {
+    }
+
+    /**
+     * Test of flip method, of class Chart.
+     */
+    @Test
+    public void testFlip() {
+    }
+
+    /**
+     * Test of getCapabilities method, of class Chart.
+     */
+    @Test
+    public void testGetCapabilities() {
+    }
 }
--- src/test/java/org/jzy3d/chart/ChartTestFailed.java
+++ src/test/java/org/jzy3d/chart/ChartTestFailed.java
@@ -2,38 +2,37 @@ package org.jzy3d.chart;
 
 import java.awt.image.BufferedImage;
 
-public class ChartTestFailed extends Exception{
-	public ChartTestFailed() {
-		super();
-	}
+public class ChartTestFailed extends Exception {
 
-	public ChartTestFailed(String message, Throwable cause) {
-		super(message, cause);
-	}
+    public ChartTestFailed() {
+        super();
+    }
 
-	public ChartTestFailed(String message) {
-		super(message);
-	}
+    public ChartTestFailed(String message, Throwable cause) {
+        super(message, cause);
+    }
 
-	public ChartTestFailed(Throwable cause) {
-		super(cause);
-	}
-	
-	public ChartTestFailed(String message, BufferedImage i1, BufferedImage i2) {
-		super(message);
-		this.i1 = i1;
-		this.i2 = i2;
-	}
+    public ChartTestFailed(String message) {
+        super(message);
+    }
 
-	public BufferedImage getImage1() {
-		return i1;
-	}
+    public ChartTestFailed(Throwable cause) {
+        super(cause);
+    }
 
-	public BufferedImage getImage2() {
-		return i2;
-	}
+    public ChartTestFailed(String message, BufferedImage i1, BufferedImage i2) {
+        super(message);
+        this.i1 = i1;
+        this.i2 = i2;
+    }
 
-	protected BufferedImage i1;
-	protected BufferedImage i2;
+    public BufferedImage getImage1() {
+        return i1;
+    }
+
+    public BufferedImage getImage2() {
+        return i2;
+    }
+    protected BufferedImage i1;
+    protected BufferedImage i2;
 }
-
