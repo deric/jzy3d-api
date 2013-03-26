f2b8ea0ba6c29270e896aa9a6a46143aa9212d03
Tomas Barton <barton.tomas@gmail.com>
2013-03-26 22:00:40 +0100

    code formatting

--- src/main/java/org/jzy3d/plot3d/primitives/ScatterMultiColor.java
+++ src/main/java/org/jzy3d/plot3d/primitives/ScatterMultiColor.java
@@ -15,95 +15,100 @@ import org.jzy3d.plot3d.rendering.legends.colorbars.ColorbarLegend;
 import org.jzy3d.plot3d.rendering.view.Camera;
 import org.jzy3d.plot3d.transform.Transform;
 
-
-
-/** 
+/**
  * A scatter plot supporting a colormap for shading each dot color and alpha.
- * 
+ *
  * @author Martin Pernollet
  *
  */
-public class ScatterMultiColor extends AbstractDrawable implements IMultiColorable{
-	public ScatterMultiColor(Coord3d[] coordinates, Color[] colors, ColorMapper mapper){
+public class ScatterMultiColor extends AbstractDrawable implements IMultiColorable {
+
+    public ScatterMultiColor(Coord3d[] coordinates, Color[] colors, ColorMapper mapper) {
         this(coordinates, colors, mapper, 1.0f);
     }
-	public ScatterMultiColor(Coord3d[] coordinates, ColorMapper mapper){
+
+    public ScatterMultiColor(Coord3d[] coordinates, ColorMapper mapper) {
         this(coordinates, null, mapper, 1.0f);
     }
-    public ScatterMultiColor(Coord3d[] coordinates, Color[] colors, ColorMapper mapper, float width){
+
+    public ScatterMultiColor(Coord3d[] coordinates, Color[] colors, ColorMapper mapper, float width) {
         bbox = new BoundingBox3d();
         setData(coordinates);
-		setColors(colors);
+        setColors(colors);
         setWidth(width);
         setColorMapper(mapper);
     }
-	    
-    public void clear(){
+
+    public void clear() {
         coordinates = null;
         bbox.reset();
     }
-    
-    public void enableColorBar(ITickProvider provider, ITickRenderer renderer){
-        setLegend( new ColorbarLegend(this, provider, renderer) );
+
+    public void enableColorBar(ITickProvider provider, ITickRenderer renderer) {
+        setLegend(new ColorbarLegend(this, provider, renderer));
         setLegendDisplayed(true);
     }
-        
+
     /* */
-    
-    public void draw(GL2 gl, GLU glu, Camera cam){
+    public void draw(GL2 gl, GLU glu, Camera cam) {
         doTransform(gl, glu, cam);
-        
-        gl.glPointSize(width);      
+
+        gl.glPointSize(width);
         gl.glBegin(GL2.GL_POINTS);
 
-        if(coordinates!=null){
-            for(Coord3d coord: coordinates){
+        if (coordinates != null) {
+            for (Coord3d coord : coordinates) {
                 Color color = mapper.getColor(coord); // TODO: should store result in the point color
                 gl.glColor4f(color.r, color.g, color.b, color.a);
                 gl.glVertex3f(coord.x, coord.y, coord.z);
             }
         }
         gl.glEnd();
-        
+
         doDrawBounds(gl, glu, cam);
     }
-    
+
     @Override
     public void applyGeometryTransform(Transform transform) {
-        for(Coord3d c: coordinates){
+        for (Coord3d c : coordinates) {
             c.set(transform.compute(c));
         }
         updateBounds();
     }
-    
+
     @Override
     public void updateBounds() {
         bbox.reset();
-        for(Coord3d c: coordinates)
+        for (Coord3d c : coordinates) {
             bbox.add(c);
+        }
     }
 
     /* */
-    
-    /** 
+    /**
      * Set the coordinates of the point.
+     *
      * @param xyz point's coordinates
      */
-    public void setData(Coord3d[] coordinates){
+    public void setData(Coord3d[] coordinates) {
         this.coordinates = coordinates;
-        
+
         bbox.reset();
-        for(Coord3d c: coordinates)
+        for (Coord3d c : coordinates) {
             bbox.add(c);
-    }    
-    public Coord3d[] getData(){
+        }
+    }
+
+    public Coord3d[] getData() {
         return coordinates;
     }
-	public void setColors(Color[] colors){
-		this.colors = colors;
-		
-		fireDrawableChanged(new DrawableChangedEvent(this, DrawableChangedEvent.FIELD_COLOR));
-	}    
+
+    public void setColors(Color[] colors) {
+        this.colors = colors;
+
+        fireDrawableChanged(new DrawableChangedEvent(this, DrawableChangedEvent.FIELD_COLOR));
+    }
+
     @Override
     public ColorMapper getColorMapper() {
         return mapper;
@@ -111,23 +116,20 @@ public class ScatterMultiColor extends AbstractDrawable implements IMultiColorab
 
     @Override
     public void setColorMapper(ColorMapper mapper) {
-        this.mapper = mapper;       
+        this.mapper = mapper;
     }
-        
+
     /**
      * Set the width of the point.
+     *
      * @param width point's width
      */
-    public void setWidth(float width){
+    public void setWidth(float width) {
         this.width = width;
     }
-    
     /* */
-    
     protected Coord3d[] coordinates;
-	protected Color[]   colors;   
-	protected float     width;
+    protected Color[] colors;
+    protected float width;
     protected ColorMapper mapper;
-    
-    
 }
